77a5096083670733e4ea6a687b542804d893d7ca
M9heb <123528241+M9heb@users.noreply.github.com>
2025-05-11 09:22:18 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 164, 80, 80),
+  ),
+  textTheme: GoogleFonts.interTextTheme(),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals app',
+      theme: theme,
+      home: const Text("Hello world"),
+    );
+  }
+}
