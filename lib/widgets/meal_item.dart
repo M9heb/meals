c66d49be4bd6873261a41557d313baee88b8e310
Mahdi Allahdadi <m.moheb1382@gmail.com>
2025-05-20 10:51:01 +0430

    Adds the meal trait widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/models/meal.dart';
+import 'package:test_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
+  @override
   Widget build(context) {
     return Card(
       margin: EdgeInsets.all(8),
@@ -47,7 +60,17 @@ class MealItem extends StatelessWidget {
                         height: 12,
                       ),
                       Row(
-                        children: [],
+                        spacing: 12,
+                        children: [
+                          MealItemTrait(
+                              icon: Icons.timer_outlined,
+                              label: '${meal.duration} min'),
+                          MealItemTrait(
+                              icon: Icons.work_outline, label: complexityText),
+                          MealItemTrait(
+                              icon: Icons.attach_money,
+                              label: affordabilityText),
+                        ],
                       )
                     ],
                   ),
---
+++ lib/widgets/meal_item_trait.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MealItemTrait extends StatelessWidget {
+  const MealItemTrait({super.key, required this.icon, required this.label});
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: 18,
+          color: Theme.of(context).colorScheme.onSurfaceVariant,
+        ),
+        SizedBox(
+          width: 8,
+        ),
+        Text(
+          label,
+          style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ],
+    );
+  }
+}
